445b057933886667dfd0a4463ffe3fa6f73a1d62
Florian Maak <fmaak@outlook.de>
2017-10-04 20:28:17 +0200

    fix(Blog): Added getRoutes-method

--- src/eWar/Blog/Blog.php
+++ src/eWar/Blog/Blog.php
@@ -10,4 +10,12 @@ use eWar\Framework\AbstractApplication;
  */
 class Blog extends AbstractApplication
 {
+    /**
+     * getRoutes
+     * @return array
+     */
+    public function getRoutes() : array
+    {
+        return (array) json_decode(file_get_contents(GLOBAL_DIR . '/config/routes.json'));
+    }
 }
